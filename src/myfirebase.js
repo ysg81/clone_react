443a93f199a13c06461bbc8139a17fd36af3ad3f
ysg81 <qpple6@naver.com>
2021-03-16 22:46:03 +0900

    Build Login&Logout

--- src/myfirebase.js
+++ src/myfirebase.js
@@ -10,8 +10,18 @@ const firebaseConfig = {
 	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 	appId: process.env.REACT_APP_APP_ID,
 };
+// const firebaseConfig = {
+// 	apiKey: 'AIzaSyCU9XAd0uLonizg8DRe61dQooKNP7Pj4Vw',
+// 	authDomain: 'clonereact-32941.firebaseapp.com',
+// 	projectId: 'clonereact-32941',
+// 	storageBucket: 'clonereact-32941.appspot.com',
+// 	messagingSenderId: '739645264148',
+// 	appId: '1:739645264148:web:ff72809f770b4c18e23201',
+// };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+export const firebaseInstance = firebase;
 
 export const authService = firebase.auth();
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,39 +1,65 @@
-import React,{ useState } from "react"
+import { authService, firebaseInstance } from "myfirebase"
+import React, { useState } from "react"
+
 
 const Auth = () => {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [newAccount, setNewAccount] = useState(false)
-    const onChange = (e) => {
-        const { name, value } = e.target;
-        if(name === "email"){ setEmail(value) }
-        else if(name === "password"){ setPassword(value) }
-    }
-    const onSubmit = (e) => {
-        e.preventDefault();
-        if(newAccount){
-            //create account
-        } else {
-            //log in
+	const [email, setEmail] = useState("")
+	const [password, setPassword] = useState("")
+	const [newAccount, setNewAccount] = useState(true)
+    const [error, setError] = useState("")
+	const onChange = (e) => {
+		const { name, value } = e.target
+		if (name === "email") {
+			setEmail(value)
+		} else if (name === "password") {
+			setPassword(value)
+		}
+	}
+	const onSubmit = async (e) => {
+		e.preventDefault()
+		try {
+            let data;
+			if (newAccount) {
+				// create account
+				data = await authService.createUserWithEmailAndPassword(
+                    email,
+                    password
+                )
+			} else {
+				// log in
+				data = await authService.signInWithEmailAndPassword(
+                    email,
+                    password
+                )
+			}
+			console.log(data)
+		} catch (err) {
+			setError(err.message)
+		}
+	}
+
+    const toggleAccount = () => { setNewAccount(prev => !prev) }
+    const onSocialClick = async (e) => {
+        const {name} = e.target;
+        let provider
+        if(name === 'google'){
+            provider = new firebaseInstance.auth.GoogleAuthProvider();
         }
+        const data = await authService.signInWithPopup(provider);
+        console.log(data);
     }
+
 	return (
 		<div>
 			<form onSubmit={onSubmit}>
-				<input
-                    name="email" type="text" placeholder="Email" required
-                    value={email} 
-                    onChange={onChange}
-                />
-				<input
-                    name="password" type="password" placeholder="Password" required
-                    value={password}
-                    onChange={onChange}
-                />
-				<input type="submit" value={newAccount ? "Create Account" : "Log In"} />
-			</form>
+				<input name="email" type="text" placeholder="Email" required value={email} onChange={onChange} />
+				<input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} />
+				<input type="submit" value={newAccount ? "Create Account" : "Sign In"} />
+			{error}
+            </form>
+            <span onClick={toggleAccount}>{newAccount ? "LogIn" : "Create Account" }</span>
 			<div>
-				<button>Continue with Google</button>
+				<button onClick={onSocialClick} name="google">Continue with Google</button>
 			</div>
 		</div>
 	)
